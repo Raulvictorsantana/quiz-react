dc9515d5a5edbba627990c0e86289450cae5c295
raul victor <79764863+Raulvictorsantana@users.noreply.github.com>
2023-07-29 15:54:20 -0300

    atualizando1

--- src/componets/Welcome.jsx
+++ src/componets/Welcome.jsx
@@ -1,18 +1,23 @@
+import { useContext } from "react";
+import { QuizContext } from "../context/quiz";
+
 import Quiz from "../img/quiz.svg"
 import "./Welcome.css";
 
 
 
+const Welcome = () => {
+  const [quizState, dispatch] = useContext(QuizContext);
 
-const Welcome = () => { 
   return (
     <div className="welcome">
       <h2>Seja bem-vindo</h2>
-      <p>Clique no botao abaixo para começar</p>
-<button>Inicar</button>
-<img src={Quiz} alt="inicio de quiz" />
+      <p>Clique no botão abaixo para começar</p>
+      <button onClick={() => console.log({ quizState, dispatch })}>Iniciar</button>
+      <img src={Quiz} alt="inicio de quiz" />
     </div>
-  )
-}
+  );
+};
+
 
 export default Welcome
--- src/context/quiz.jsx
+++ src/context/quiz.jsx
@@ -1,2 +1,34 @@
-import { createContext } from "react";
-export const QuizContext =createContext()
+import { createContext, useReducer } from "react";
+import PropTypes from 'prop-types';
+import question from "../data/question"
+
+
+const STAGES = ["start", "playing", "end"]
+
+const initialState = {
+    gameStage: STAGES[0],
+    question
+}
+
+const quisReducer = (state, action) => {
+
+    switch (action.type) {
+        case "CHANGE_STATE":
+            return state;
+
+        default:
+            return state;
+    }
+
+};
+export const QuizContext = createContext();
+
+export const QuizProvider = ({ children }) => {
+    const value = useReducer(quisReducer, initialState);
+    return <QuizContext.Provider value={value}> {children} </QuizContext.Provider>
+
+}
+
+QuizProvider.propTypes = {
+    children: PropTypes.node.isRequired,
+};
